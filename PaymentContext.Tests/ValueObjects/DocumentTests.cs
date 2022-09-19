cdbd146442a70cd37b6b1bc66b6264f0cb32fb57
Georgeton <georgeton.santos.santana@gmail.com>
2022-09-19 17:28:25 -0300

    Tests
    Test ValueObjects

---
+++ PaymentContext.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Enums;
+using PaymentContext.Domain.ValueObjects;
+
+namespace PaymentContext.Tests.ValueObjects;
+
+[TestClass]
+public class DocumentTests
+{
+    [TestMethod]
+    public void RetornarErroQuandoCNPJForInvalido()
+    {
+        var doc = new Document("123", EDocumentType.CNPJ);
+        Assert.IsTrue(doc.Invalid);
+    }
+
+    [TestMethod]
+    public void RetornarSucessoQuandoCNPJForValido()
+    {
+        var doc = new Document("63814003000189", EDocumentType.CNPJ);
+        Assert.IsTrue(doc.Valid);
+    }
+
+    [TestMethod]
+    public void RetornarErroQuandoCPFForInvalido()
+    {
+        var doc = new Document("1234", EDocumentType.CPF);
+        Assert.IsTrue(doc.Invalid);
+    }
+
+    [TestMethod]
+    [DataTestMethod]
+    [DataRow("44645422049")]
+    [DataRow("23017962059")]
+    [DataRow("45067710012")]
+    public void RetornarSucessoQuandoCPFForValido(string cpf)
+    {
+        var doc = new Document(cpf, EDocumentType.CPF);
+        Assert.IsTrue(doc.Valid);
+    }
+}
